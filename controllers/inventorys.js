7eb51b3ff6b56dc4f15a38893d99b2592609235f
mlinares <m.linares93@gmail.com>
2024-09-20 23:10:38 -0400

    create inventory controller index function

--- controllers/inventorys.js
+++ controllers/inventorys.js
@@ -1,1 +1,15 @@
 import { Inventory } from "../models/inventory"; 
+
+async function index(req, res){
+  try {
+    const workshops = await Inventory.find({})
+    res.status(200).json(workshops)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
+
+export {
+  index,
+}

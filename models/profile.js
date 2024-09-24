310e24b6c9d61ef317819a53d70e8ea51c8cc9d4
mlinares <m.linares93@gmail.com>
2024-09-23 23:19:04 -0400

    add orders property to profile model

--- models/profile.js
+++ models/profile.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema
 
 const profileSchema = new Schema({
   name: String,
-  photo: String
+  photo: String,
+  orders: { type: Schema.Types.ObjectId, ref: 'Order'}
 },{
   timestamps: true,
 })

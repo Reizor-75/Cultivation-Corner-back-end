cdfe9429ca7d2137d48b7ef36513e51e910551b7
mlinares <m.linares93@gmail.com>
2024-09-23 23:20:36 -0400

    create order create controller function

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,5 +1,16 @@
 import { Order } from '../models/order.js'
 
-export {
+async function create(req, res){
+  try{
+    const order = await Order.create(req.body)
+    res.status(201).json(order)
+  }
+  catch (err){    
+    console.log(err)
+    res.status(500).json(err)
+  }
+}
 
+export {
+  create,
 }

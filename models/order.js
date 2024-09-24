b0d69f19c8098567fdaffacd1f14556e746784d0
mlinares <m.linares93@gmail.com>
2024-09-23 23:14:31 -0400

    create order model

---
+++ models/order.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const orderSchema = new Schema({
+  orderNumber: { type: String, required: true },
+  date: { type: Date, required: true },
+  orderList: [{ type: Schema.Types.ObjectId, ref: 'Product'}],
+  delivery: {
+    type: String,
+    enum:['pick up', 'shipping', 'delivery' ],
+    default: 'shipping'
+  },
+  fulfilled : { type: Boolean, default: false},
+  address: { type: String }
+},{
+  timestamps: true,
+})
+
+const Order = mongoose.model('order', orderSchema)
+
+export { Order }

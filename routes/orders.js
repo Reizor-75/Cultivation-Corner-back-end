8639518a1bc049d3884ee61b1196f171b6106f88
mlinares <m.linares93@gmail.com>
2024-09-23 23:24:00 -0400

    create order create route

--- routes/orders.js
+++ routes/orders.js
@@ -6,9 +6,9 @@ import * as ordersCtrl from '../controllers/orders.js'
 const router = Router()
 
 /*---------- Public Routes ----------*/
+router.post('/newOrder', ordersCtrl.create)
 
 /*---------- Protected Routes ----------*/
-
 
 
 export { router }

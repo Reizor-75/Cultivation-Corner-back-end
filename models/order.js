8a7a9b8b13f9ba4652a58663ceac5d88bba7610d
mlinares <m.linares93@gmail.com>
2024-09-25 00:41:38 -0400

    update order model

--- models/order.js
+++ models/order.js
@@ -5,14 +5,17 @@ const Schema = mongoose.Schema
 const orderSchema = new Schema({
   recipient: { type: Schema.Types.ObjectId, ref: 'Profile'},
   orderNumber: { type: String, required: true },
-  date: { type: Date, required: true },
   orderList: [{ type: Schema.Types.ObjectId, ref: 'Product'}],
   delivery: {
     type: String,
     enum:['pick up', 'shipping', 'delivery' ],
     default: 'shipping'
   },
-  fulfilled : { type: Boolean, default: false},
+  fulfilled : {
+    type: String,
+    enum:['canceled', 'pending', 'completed' ],
+    default: 'pending'
+  },
   address: { type: String }
 },{
   timestamps: true,

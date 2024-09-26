5ade79c6c2e41b29b140d794a589f3a003f0bb18
mlinares <m.linares93@gmail.com>
2024-09-26 00:00:51 -0400

    create blogPost create controller function

--- controllers/blogPosts.js
+++ controllers/blogPosts.js
@@ -10,6 +10,16 @@ async function index(req, res){
   }
 }
 
+async function create(req, res){
+  try {
+    const post = await BlogPost.create(req.body)
+    res.status(200).json(post)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
+}
+
 export{
   index,
 }

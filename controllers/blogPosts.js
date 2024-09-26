c69cc24ef058c4c2269e17c749f5b535cc10eaf0
mlinares <m.linares93@gmail.com>
2024-09-25 23:56:55 -0400

    create blogPost index controller function

--- controllers/blogPosts.js
+++ controllers/blogPosts.js
@@ -1,5 +1,15 @@
 import { BlogPost } from '../models/blogPost.js'
 
+async function index(req, res){
+  try {
+    const post = await BlogPost.find({}).populate(['author', 'productList'])
+    res.status(200).json(post)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
 
 export{
+  index,
 }

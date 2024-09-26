419795622edddcbf6fb3d64d15d1057b351649b0
mlinares <m.linares93@gmail.com>
2024-09-26 00:16:10 -0400

    create comment embedded schema

--- models/blogPost.js
+++ models/blogPost.js
@@ -2,11 +2,20 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const commentSchema= new Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  commenter: { type: Schema.Types.ObjectId, ref: 'Profile' }
+}, {
+  timestamps: true
+})
+
 const blogPostSchema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: 'Profile'},
   postTitle: { type: String, required: true },
   content: { type: String, required: true },
   productList: [{ type: Schema.Types.ObjectId, ref: 'Product'}],
+  comments: [commentSchema],
 },{
   timestamps: true,
 })

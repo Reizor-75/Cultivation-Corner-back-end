bf26a21d6ab8acd398b7cb199be83bf2df651b81
mlinares <m.linares93@gmail.com>
2024-10-01 11:05:42 -0400

    create showEmployee and updateRole Routes

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,15 +1,16 @@
 import { Router } from 'express'
-import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
+import { decodeUserFromToken, checkAuth, checkOwner } from '../middleware/auth.js'
 import * as profilesCtrl from '../controllers/profiles.js'
 
 const router = Router()
 
 /*---------- Public Routes ----------*/
-
+router.get('/aboutUs', profilesCtrl.showEmployee)
 
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
 router.get('/', checkAuth, profilesCtrl.index)
 router.put('/:id/add-photo', checkAuth, profilesCtrl.addPhoto)
+router.put(':profileId/updateRole', checkOwner, profilesCtrl.updateRole)
 
 export { router }
